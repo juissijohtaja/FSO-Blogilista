14d9deeaad27623eee485e5c38abb8f441413540
JuissiJohtaja <juha.koivu@outlook.com>
2019-09-19 16:39:23 +0300

    uibuild

---
+++ build/precache-manifest.37606b85a2d5b54574bbd740783305b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8aae743520861c7fce44",
+    "url": "/static/js/main.408c29ac.chunk.js"
+  },
+  {
+    "revision": "54421f62b43a7ed46f48",
+    "url": "/static/js/2.26fdf861.chunk.js"
+  },
+  {
+    "revision": "8aae743520861c7fce44",
+    "url": "/static/css/main.b2ed8f31.chunk.css"
+  },
+  {
+    "revision": "c417a2e44fc5a1edf1103fa60c32206b",
+    "url": "/index.html"
+  }
+];
